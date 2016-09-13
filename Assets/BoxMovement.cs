d8e63641689f661fc2e671ef18eb8b0caf47e450
jannef <janne.forsell@cs.tamk.fi>
2016-09-13 18:05:27 +0300

    Initial commit on work started on monday 2016-09-11. Basic interactable block. Class refactoring pending.

---
+++ Assets/BoxMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoxMovement : MonoBehaviour {
+	public boxmovementclass _movementClass;
+
+	void Awake() {
+		_movementClass = new BasicUpAndDownBox(this);
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		var colYLoc = _movementClass._transform.position.y - col.transform.position.y;
+
+		if (colYLoc > 0) {
+			_movementClass.OnBelowColl (col.collider.gameObject.tag, col.collider.gameObject.GetComponent<BoxMovement> ()._movementClass._speed);
+		} else {
+			_movementClass.OnTopColl (col.collider.gameObject.tag, col.collider.gameObject.GetComponent<BoxMovement> ()._movementClass._speed);
+		}
+	}
+
+	void Update() {
+		_movementClass.MoveBlock ();
+	}
+
+	private void OnMouseDown() {
+		_movementClass.OnBoxClicked ();
+	}
+
+}
---
+++ Assets/boxmovementclass.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boxmovementclass {
+	public Transform _transform;
+	public float _speed = 2.0f;
+	protected MonoBehaviour _master;
+
+	public boxmovementclass (MonoBehaviour master) {
+		_master = master;
+		_transform = _master.transform;
+	}
+
+	void Awake() {
+		_transform = _master.transform;
+	}
+
+
+
+	public virtual void OnBelowColl (string tag, float speed) {
+	}
+
+	public virtual void OnTopColl(string tag, float speed) {
+	}
+
+	void Update() {
+		MoveBlock ();
+	}
+
+	public virtual void MoveBlock() {
+
+	}
+
+	private void OnMouseDown() {
+		OnBoxClicked ();
+	}
+
+	public virtual void OnBoxClicked() {
+	}
+}
