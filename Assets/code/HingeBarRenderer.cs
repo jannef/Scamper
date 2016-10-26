015bcf43c9aea22ac81088d490bcce807a722b26
Janne Forsell <jannef1337@gmail.com>
2016-10-26 10:28:19 +0300

    Added linerenderer to hinged hanger thing. Still need to work on it a little to make it a little more realistic looking.

---
+++ Assets/code/HingeBarRenderer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace fi.tamk.game.theone.phys
+{
+    [ExecuteInEditMode]
+    public class HingeBarRenderer : MonoBehaviour
+    {
+        [SerializeField] private Transform AnchorPos;
+        [SerializeField] private Transform ObjectPos;
+
+        private LineRenderer _lr;
+
+        private void Awake()
+        {
+            _lr = GetComponent<LineRenderer>();
+            UpdatePositions();
+        }
+
+        private void UpdatePositions()
+        {
+            _lr.SetPositions(new[] { AnchorPos.position, ObjectPos.position });
+        }
+
+        private void Update()
+        {
+            UpdatePositions();
+        }
+    }
+}
