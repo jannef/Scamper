25448c9961c17a09d6ee7a4ea01cf6cd8714e194
unknown <c5jforse@PELIKONE8-PC.wpk.tpu.fi>
2016-10-27 13:34:32 +0300

    Updated roped box a little. Added code to display Unity ads on Android and iOS.

---
+++ Assets/code/ads/AdManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEditor.Advertisements;
+using System.Collections;
+
+public class AdManager : MonoBehaviour {
+    [SerializeField] private string AndroidId = "1181015";
+
+    private void Start()
+    {
+        Advertisement.Initialize(AndroidId, true);
+    }
+
+    public void DisplayAdVideo()
+    {
+        if (Advertisement.IsReady("video") && !Advertisement.isShowing)
+        {
+            Advertisement.Show("video", new ShowOptions { resultCallback = HandleClosingOfAd });
+        }
+    }
+
+    private void HandleClosingOfAd(ShowResult result)
+    {
+        Debug.Log(string.Format("{0}", result.ToString()));
+    }
+}
