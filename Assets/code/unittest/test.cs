75574d5b87b75b3792d01aa20273f301d24fab45
jannef <janne.forsell@cs.tamk.fi>
2016-11-24 17:35:01 +0200

    Revert "Debugging of android file save done. Fixed."
    This reverts commit 085d8812a9c87d1e13516b7a50ec955c456b015f.

---
+++ Assets/code/unittest/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using fi.tamk.game.theone.menu;
+
+public class test : MonoBehaviour {
+    public LevelLoadController Control;
+    private Text _text;
+
+	// Use this for initialization
+	void Start () {
+        _text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _text.text = string.Format("{0}", Control._saveData.RandomTest);
+	}
+}
