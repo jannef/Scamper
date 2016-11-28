3778de3ba1fb830bc4253e35906e47a843c69e74
Veikka Saaristo <veikka.saaristo@hotmail.com>
2016-11-28 15:14:33 +0200

    + Fixed Main manu button scaling + Modified levelSelect, now has four buttons that lead to 4 game scenes correctly (4th obviously still missing) * Needs only graphics and minor tweaks when they're in place

---
+++ Assets/code/LoadOnClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+namespace fi.tamk.game.theone.menu
+{
+
+    public class LoadOnClick : MonoBehaviour
+    {
+
+        public void LoadScene(int level)
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+}
